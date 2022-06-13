b606bb5097ff49f184c30b1575903e319be9fdf7
hsosa9 <hectorsosa91mail.com>
2022-06-12 23:27:22 -0600

    Cambios

--- app/src/main/java/com/example/pmo12pp1/ActivityList.java
+++ app/src/main/java/com/example/pmo12pp1/ActivityList.java
@@ -2,15 +2,21 @@ package com.example.pmo12pp1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.pmo12pp1.Procesos.Empleados;
 import com.example.pmo12pp1.Procesos.SQLliteConexion;
 import com.example.pmo12pp1.Procesos.Transacciones;
+import com.google.android.material.snackbar.Snackbar;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class ActivityList extends AppCompatActivity {
@@ -28,6 +34,30 @@ public class ActivityList extends AppCompatActivity {
         listemple = (ListView) findViewById(R.id.listviewEmple);
         conexion = new SQLliteConexion(this, Transacciones.NameDataBase, null, 1);
         ObtenerListaEmpleados();
+
+        ArrayAdapter adp = new ArrayAdapter(this, android.R.layout.simple_list_item_1,ArregloEmpleados);
+        listemple.setAdapter(adp);
+
+        listemple.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String Info = "ID : " + listaempleados.get(position).getId() + "\n" +
+                        listaempleados.get(position).getNombres();
+
+                Snackbar.make(view, Info,Snackbar.LENGTH_LONG).show();
+
+                Intent intetShare = new Intent();
+                intetShare.setAction(Intent.ACTION_SEND);
+                intetShare.putExtra(Intent.EXTRA_TEXT, Info);
+                intetShare.setType("text/plain");
+                Intent Share = Intent.createChooser(intetShare, null);
+                startActivity(Share);
+
+            }
+        });
+
+
     }
 
     private void ObtenerListaEmpleados() {
